353c730661b8089849816c48cfbb7682c7a18d50
sowmyasree_v <sowmya.s@accubits.com>
2022-08-26 19:20:17 +0530

    message added

--- app.js
+++ app.js
@@ -210,7 +210,7 @@ const server = app.listen(port, () =>
   winston.info(`Listening on port ${port}...`)
 );
 app.get("/messages", (req, res) => {
-  res.send("Hello Codegene");
+  res.send("Hello Codegene company");
 });
 
 module.exports = server;
