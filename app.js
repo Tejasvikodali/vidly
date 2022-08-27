7e1df766b3f9d16a0e0e1179c8919eca977e05d7
sowmyasree_v <sowmya.s@accubits.com>
2022-08-27 12:27:57 +0530

    home route

--- app.js
+++ app.js
@@ -138,8 +138,23 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-http = require("http").Server(app);
+//http = require("http").Server(app);
+const http = require("http"); // Loads the http module
 
+http
+  .createServer((request, response) => {
+    // 1. Tell the browser everything is OK (Status code 200), and the data is in plain text
+    // response.writeHead(200, {
+    //     'Content-Type': 'text/plain'
+    // });
+
+    // 2. Write the announced text to the body of the page
+    response.write("Hello, codegene!");
+
+    // 3. Tell the server that all of the response headers and body have been sent
+    response.end();
+  })
+  .listen(1337); // 4. Tells the server what port to be on
 // app.get("/messages", (req, res) => {
 //   res.send("Hello Codegene!");
 // });
