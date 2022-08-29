452d44798b77c59950492d60b3e214e6e6ca3d01
sowmyasree_v <sowmya.s@accubits.com>
2022-08-29 17:14:22 +0530

    first msg

--- routes/task.js
+++ routes/task.js
@@ -5,6 +5,6 @@ const router = express.Router();
 router.post("/test", async (req, res) => {
   var data = req.body.name;
   console.log(req.body);
-  return res.send(data.name);
+  return res.send(data);
 });
 module.exports = router;
